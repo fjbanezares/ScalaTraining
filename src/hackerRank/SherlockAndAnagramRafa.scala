fcbd3ec4347277e11f2e511fe419d4d18ba222fa
Rafael De la Cueva <rafael.cueva@contractor.isbanuk.com>
2018-11-25 23:22:25 +0000

    rafas version for SherlockAndAnagram

---
+++ src/hackerRank/SherlockAndAnagramRafa.scala
@@ -0,0 +1,40 @@
+﻿package hackerRank
+
+import com.google.common.math.BigIntegerMath
+import scala.collection.mutable
+
+object SherlockAndAnagrams extends App{
+
+  def sherlockAndAnagrams(s: String): Int = {
+    val sylabs = mutable.Map[mutable.Map[Char, Int],Int]()
+    for(i <- s.indices){
+      var j = i+1
+      while(j <= s.length){
+        val substFirma = calculateFirma(s.substring(i,j))
+        if(sylabs.keySet.contains(substFirma)){
+          sylabs(substFirma) += 1
+        }
+        else{
+          sylabs += substFirma -> 1
+        }
+        j+=1
+      }
+    }
+    var counter = 0
+    for((key,value) <- sylabs if value >= 2 ){
+      counter = counter + BigIntegerMath.binomial(value,2).intValue()
+    }
+  counter
+  }
+
+  def calculateFirma(word: String): mutable.Map[Char, Int] ={
+    val firma = mutable.Map[Char, Int]()
+    for(c <- word){
+      if(firma.keySet.contains(c)){
+        firma(c) += 1
+      }
+      else firma += c -> 1
+    }
+    firma
+  }
+}
